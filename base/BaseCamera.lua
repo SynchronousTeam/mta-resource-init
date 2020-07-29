730e0729272d13d83a4f143aa5b31d6f667182ef
Kyonax <kyonax25@gmail.com>
2020-07-29 00:28:22 -0500

    Clean & Optimize | Code and Variables

--- base/BaseCamera.lua
+++ base/BaseCamera.lua
@@ -16,26 +16,29 @@
 ---•---•---•---•---•---•---•---•---•---•---•---•---•---•---•---•---•---•---•---•---•---
 --- Client Variables
 event = {add = addEventHandler, load = addEvent, execute = triggerServerEvent} -- Make simple calls for addEvent and triggerServerEvent by the variable event
-POINT_X, POINT_Y, POINT_Z = 1488, 250, 53 -- Set the Default Camera Position
-ROTATION_X, ROTATION_Y, ROTATION_Z = 0, 90, -65 -- Set the Default Camera Rotation
+local const = {
+    position = {X = 1488, Y = 250, Z = 53},
+    rotation = {X = 0, Y = 90, Z = -65},
+    fx = {CAMERA_IN_OPACITY = 3}
+}
+local base_component = {"radar", "area_name"}
 --- Functions
-function onBaseCamera(POINT_X, POINT_Y, POINT_Z, ROTATION_X, ROTATION_Y,
-                      ROTATION_Z)
-    setPlayerHudComponentVisible("radar", false)
-    setPlayerHudComponentVisible("area_name", false)
+local function onBaseCamera(POSITION_X, POSITION_Y, POSITION_Z, ROTATION_X,
+                            ROTATION_Y, ROTATION_Z, IN_OPACITY)
+
+    for i = 1, table.getn(base_component) do
+        setPlayerHudComponentVisible(base_component[i], false)
+    end -- This for removes all the HUD Components catched from the array base_component
     showCursor(true)
     showChat(false)
-    fadeCamera(true, 3) -- Slowly fade the camera in to make the screen visible 
-    setCameraMatrix(POINT_X, POINT_Y, POINT_Z, ROTATION_X, ROTATION_Y,
+    fadeCamera(true, IN_OPACITY) -- Slowly fade the camera in to make the screen visible 
+    setCameraMatrix(POSITION_X, POSITION_Y, POSITION_Z, ROTATION_X, ROTATION_Y,
                     ROTATION_Z) -- set the player's camera to a fixed position, looking at a fixed point
 end -- Function that initialice the Register Base
 --- Events Created
-event.load('base-camera:sendData', true)
-event.add('base-camera:sendData', root, function()    
-    event.execute('base-backend:execute', resourceRoot)
-end, true) -- Get the JavaScript Call
-
 event.load('base-camera:on', true)
-event.add('base-camera:on', root, function()    
-    onBaseCamera(POINT_X, POINT_Y, POINT_Z, ROTATION_X, ROTATION_Y, ROTATION_Z)
+event.add('base-camera:on', root, function()
+    onBaseCamera(const.position.X, const.position.Y, const.position.Z,
+                 const.rotation.X, const.rotation.Y, const.rotation.Z,
+                 fx.CAMERA_IN_OPACITY)
 end, true) -- An event that turns ON the Camera and taks a City shot 
